a89236768778939b05562ad294ede41cb1c178aa
Ahmed Zouaghi <ahmedzouaghi2003@gmail.com>
2024-02-06 01:50:20 +0100

    this app add, subtract and reset

---
+++ hellotoast_Add_Subtract_Reset/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount=0;
+    private TextView mShowCount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, "This app add, subtract and reset",
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+
+
+    public void countUp(View view) {
+        ++mCount;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+
+    public void countDown(View view) {
+        --mCount;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+
+    public void reset(View view) {
+        mCount = 0;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+}
